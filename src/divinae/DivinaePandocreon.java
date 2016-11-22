fe05a926dc468a2e6470c0df7c4b5541681b9799
Folow <davidgalante77@gmail.com>
2016-11-22 01:30:44 +0100

    Ajout du main avec les choix blabla Et sinon tu aimes NF16? 1-------Ouai 2-------Non

--- src/divinae/DivinaePandocreon.java
+++ src/divinae/DivinaePandocreon.java
@@ -1,14 +1,31 @@
 package divinae;
 
+import java.util.Scanner;
+
 /**
  * Created by david on 21/11/2016.
  */
 public class DivinaePandocreon {
 
-
-
     public static void main(String[] args) {
+        boolean creerpartie = false;
+        Scanner choixUtilisateur = new Scanner(System.in);
+        int choix;
+        do {
+            System.out.println("----------DIVINAE PANDOCREON----------\n----------1-Créer Partie\n----------0-Exit");
+            choix = choixUtilisateur.nextInt();
+        } while (choix > 1 || choix < 0);
 
-        Partie partie=Partie.getInstance();
+        switch(choix)
+        {
+            case 1:
+                Partie partie = Partie.getInstance();
+                break;
+            default:
+                System.out.println("A la prochaine");
+                return;
+
+        }
+        return;
     }
 }
--- src/divinae/Partie.java
+++ src/divinae/Partie.java
@@ -1,11 +1,12 @@
 package divinae;
 
-import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+import java.util.Scanner;
 
 /**
  * Created by david on 21/11/2016.
  */
 public class Partie {
+    private Joueur[] joueur;
     private static Partie ourInstance = null;
 
     public static Partie getInstance()
@@ -16,9 +17,18 @@ public class Partie {
        }
         return ourInstance;
     }
-
+    public void choisirJoueur() {
+        Scanner choixUtilisateur = new Scanner(System.in);
+        int nbJoueur;
+        do {
+            System.out.println("Veuillez choisir le nombre de joueur");
+            nbJoueur = choixUtilisateur.nextInt();
+        } while (nbJoueur > 2 && nbJoueur < 7);
+        this.joueur=new Joueur[nbJoueur];
+    }
     private Partie()
     {
         System.out.println("Partie créée");
     }
+
 }
